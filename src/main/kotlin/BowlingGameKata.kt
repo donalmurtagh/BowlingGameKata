bef0e5619696959a0eccc954876e027841eb69cb
Dave Mulligan <dave@mulligan.ie>
2019-05-01 23:16:57 +0100

    Solution complated as a pair

--- src/main/kotlin/BowlingGameKata.kt
+++ src/main/kotlin/BowlingGameKata.kt
@@ -1,6 +1,23 @@
+fun processGame(game: Array<Int>): Int {
+    var gameIndex = 0
 
-class BowlingGameKata {
+    return (0..9).map {
+        val score1 = game[gameIndex++]
 
-    fun testSomething(str: String) = "XX-$str-XX"
+        val score2 = if (score1 != 10) game[gameIndex++] else 0
 
+        val bonus = when {
+            // A strike is scored.
+            (score1 == 10) -> game[gameIndex] + game[gameIndex + 1]
+
+            // A spare is scored.
+            (score1 + score2 == 10) -> game[gameIndex]
+
+            // No bonus score :(
+            else -> 0
+        }
+
+        score1 + score2 + bonus
+    }.sum()
 }
+
--- src/test/kotlin/BowlingGameKataTests.kt
+++ src/test/kotlin/BowlingGameKataTests.kt
@@ -6,16 +6,175 @@ import org.junit.Test
 class BowlingGameKataTests {
 
     @Test
-    fun `testSomething - Should return string wrapped with XX`() {
-        // When
-        val valueToTest = "SomethingToTest"
-        val expectedResult = "XX-$valueToTest-YY"
+    fun `processGame - Should return a zero score - When all gutter balls`() {
+        val game = arrayOf(
+                0, 0, // Frame 1
+                0, 0, // Frame 2
+                0, 0, // Frame 3
+                0, 0, // Frame 4
+                0, 0, // Frame 5
+                0, 0, // Frame 6
+                0, 0, // Frame 7
+                0, 0, // Frame 8
+                0, 0, // Frame 9
+                0, 0 // Frame 10
+        )
 
-        // Given
-        val result = BowlingGameKata().testSomething(valueToTest)
+        val result = processGame(game)
+        assertThat(result, equalTo(0))
+    }
 
-        // Then
-        assertThat(result, equalTo(expectedResult))
+    @Test
+    fun `processGame - Should return correct score - When 9 pins down each round`() {
+        val game = arrayOf(
+                9, 0, // Frame 1
+                9, 0, // Frame 2
+                9, 0, // Frame 3
+                9, 0, // Frame 4
+                9, 0, // Frame 5
+                9, 0, // Frame 6
+                9, 0, // Frame 7
+                9, 0, // Frame 8
+                9, 0, // Frame 9
+                9, 0 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(90))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points for a spare - When scored other than end of game`() {
+        val game = arrayOf(
+                5, 5, // Frame 1
+                5, 0, // Frame 2
+                0, 0, // Frame 3
+                0, 0, // Frame 4
+                0, 0, // Frame 5
+                0, 0, // Frame 6
+                0, 0, // Frame 7
+                0, 0, // Frame 8
+                0, 0, // Frame 9
+                0, 0 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(20))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points for a spare - When scored at end of game`() {
+        val game = arrayOf(
+                0, 0, // Frame 1
+                0, 0, // Frame 2
+                0, 0, // Frame 3
+                0, 0, // Frame 4
+                0, 0, // Frame 5
+                0, 0, // Frame 6
+                0, 0, // Frame 7
+                0, 0, // Frame 8
+                0, 0, // Frame 9
+                5, 5, 5 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(15))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points for a spare - When scored in every frame`() {
+        val game = arrayOf(
+                5, 5, // Frame 1
+                5, 5, // Frame 2
+                5, 5, // Frame 3
+                5, 5, // Frame 4
+                5, 5, // Frame 5
+                5, 5, // Frame 6
+                5, 5, // Frame 7
+                5, 5, // Frame 8
+                5, 5, // Frame 9
+                5, 5, 5 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(150))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points for a strike - When scored other than end of game`() {
+        val game = arrayOf(
+                10, // Frame 1
+                0, 5, // Frame 2
+                5, 0, // Frame 3
+                0, 0, // Frame 4
+                0, 0, // Frame 5
+                0, 0, // Frame 6
+                0, 0, // Frame 7
+                0, 0, // Frame 8
+                0, 0, // Frame 9
+                0, 0 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(25))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points for a strike - When scored at end of game`() {
+        val game = arrayOf(
+                0, 0, // Frame 1
+                0, 0, // Frame 2
+                0, 0, // Frame 3
+                0, 0, // Frame 4
+                0, 0, // Frame 5
+                0, 0, // Frame 6
+                0, 0, // Frame 7
+                0, 0, // Frame 8
+                0, 0, // Frame 9
+                10, 10, 10 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(30))
+    }
+
+    @Test
+    fun `processGame - Should award bonus points - When a perfect game`() {
+        val game = arrayOf(
+                10, // Frame 1
+                10, // Frame 2
+                10, // Frame 3
+                10, // Frame 4
+                10, // Frame 5
+                10, // Frame 6
+                10, // Frame 7
+                10, // Frame 8
+                10, // Frame 9
+                10, 10, 10 // Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(300))
+    }
+
+
+    @Test
+    fun `processGame - Should award bonus points - When scored both spares and strikes`() {
+        val game = arrayOf(
+                10, // Frame 1
+                5, 5, // Frame 2
+                10, // Frame 3
+                10, // Frame 4
+                10, // Frame 5
+                10, // Frame 6
+                10, // Frame 7
+                10, // Frame 8
+                10, // Frame 9
+                10, 10, 10// Frame 10
+        )
+
+        val result = processGame(game)
+        assertThat(result, equalTo(280))
     }
 
 }
